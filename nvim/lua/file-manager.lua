e2d37e9841d6204aa5170cb48ce0cf965ee5631b
norlock <jwillems25@hotmail.com>
2023-07-09 23:33:56 +0200

    update file-manager

--- nvim/lua/file-manager.lua
+++ nvim/lua/file-manager.lua
@@ -1,6 +1,19 @@
-local plenary = require("plenary")
+local path = require("plenary.path")
 --local user_win = vim.api.nvim_get_current_win()
 -- TODO remember position row position in buffer when navigating
+
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
 
 local function debug(val)
     print(vim.inspect(val))
@@ -38,7 +51,8 @@ local function openNavigation()
     local state = {
         buf = vim.api.nvim_create_buf(false, true),
         is_open = false,
-        os = vim.loop.os_uname().sysname
+        os = vim.loop.os_uname().sysname,
+        history = {},
     }
 
     local cmd = {
@@ -61,22 +75,34 @@ local function openNavigation()
 
     local function closeNavigation()
         if state.is_open then
-            vim.api.nvim_win_close(state.win, false)
+            vim.api.nvim_win_close(state.win_id, false)
             state.is_open = false
         end
     end
 
-    -- TODO mooier maken
-    local function openFile(cmd_str)
-        local cursor = vim.api.nvim_win_get_cursor(state.win)
+    local function setWindowCursor()
+        for i, buf_dir_name in ipairs(state.buf_content) do
+            for _, his_event in pairs(state.history) do
+                if buf_dir_name == his_event.next_dir_name then
+                    --debug(k)
+                    vim.api.nvim_win_set_cursor(state.win_id, { i, 0 })
+                    return
+                end
+            end
+        end
+    end
+
+    local function actionOnItem(cmd_str)
+        local cursor = vim.api.nvim_win_get_cursor(state.win_id)
         local item = state.buf_content[cursor[1]]
 
         if isItemDirectory(item) then
             if cmd_str == cmd.open then
                 setBufferContent(state.dir_path .. item)
+                setWindowCursor()
             end
         else
-            local file_rel = plenary.path:new(state.dir_path .. item):make_relative()
+            local file_rel = path:new(state.dir_path .. item):make_relative()
             closeNavigation()
             vim.cmd(cmd_str .. ' ' .. file_rel)
         end
@@ -91,13 +117,49 @@ local function openNavigation()
     end
 
     local function navigateToParent()
-        local parentDirectory = plenary.path:new(state.dir_path):parent()
+        -- aanpassen aan current 
+        local cursor = vim.api.nvim_win_get_cursor(state.win_id)
+        local current_dir_abs = path:new(state.dir_path):absolute()
 
-        if parentDirectory == "/" then
-            setBufferContent(parentDirectory)
-        else
-            setBufferContent(parentDirectory .. "/")
+        if current_dir_abs == "/" then
+            return
         end
+
+        local parent_dir_abs = path:new(current_dir_abs):parent():absolute()
+        local next_dir_name
+        --local next_dir_name = state.buf_content[cursor[1]]
+
+        if parent_dir_abs ~= "/" then
+            parent_dir_abs = parent_dir_abs .. "/"
+            next_dir_name = current_dir_abs:gsub("%" .. parent_dir_abs, "")
+        else
+            next_dir_name = current_dir_abs:sub(2)
+        end
+
+        local function getHistoryIndex()
+            for i,v in ipairs(state.history) do
+                if v.parent_dir_abs == parent_dir_abs then
+                    return i
+                end
+            end
+            return -1
+        end
+
+        local his_index = getHistoryIndex()
+
+        if his_index == -1 then
+            table.insert(state.history, {
+                parent_dir_abs = parent_dir_abs,
+                next_dir_name = next_dir_name,
+            })
+        else
+            state.history[his_index].next_dir_name = next_dir_name
+        end
+
+        debug(dump(next_dir_name))
+
+        setBufferContent(parent_dir_abs)
+        setWindowCursor()
     end
 
     local function init()
@@ -132,18 +194,18 @@ local function openNavigation()
             title_pos = 'center',
         }
 
-        state.win = vim.api.nvim_open_win(state.buf, true, options)
+        state.win_id = vim.api.nvim_open_win(state.buf, true, options)
         state.is_open = true;
 
-        vim.api.nvim_win_set_option(state.win, 'winhighlight', 'NormalFloat:Normal,FloatBorder:Normal')
+        vim.api.nvim_win_set_option(state.win_id, 'winhighlight', 'NormalFloat:Normal,FloatBorder:Normal')
 
         local buffer_options = { silent = true, buffer = state.buf }
         vim.keymap.set('n', 'q', closeNavigation, buffer_options)
-        vim.keymap.set('n', '<Cr>', function() openFile(cmd.open) end, buffer_options)
-        vim.keymap.set('n', '<Right>', function() openFile(cmd.open) end, buffer_options)
-        vim.keymap.set('n', 'v', function() openFile(cmd.vSplit) end, buffer_options)
-        vim.keymap.set('n', 'h', function() openFile(cmd.hSplit) end, buffer_options)
-        vim.keymap.set('n', 't', function() openFile(cmd.openTab) end, buffer_options)
+        vim.keymap.set('n', '<Cr>', function() actionOnItem(cmd.open) end, buffer_options)
+        vim.keymap.set('n', '<Right>', function() actionOnItem(cmd.open) end, buffer_options)
+        vim.keymap.set('n', 'v', function() actionOnItem(cmd.vSplit) end, buffer_options)
+        vim.keymap.set('n', 'h', function() actionOnItem(cmd.hSplit) end, buffer_options)
+        vim.keymap.set('n', 't', function() actionOnItem(cmd.openTab) end, buffer_options)
         vim.keymap.set('n', '<Left>', navigateToParent, buffer_options)
     end
 

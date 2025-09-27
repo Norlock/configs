702a157d4b01d0b62b6f65ac907aef3095e3ebcc
norlock <jwillems25@hotmail.com>
2025-09-27 23:39:11 +0700

    add niri and hail-bar

---
+++ hail-bar/src/main.rs
@@ -0,0 +1,438 @@
+use chrono::{DateTime, Local};
+use iced::border::Radius;
+use iced::futures::io;
+use iced::time::milliseconds;
+use iced::widget::{Button, Container, button, container, row, svg, text};
+use iced::{
+    Alignment, Background, Border, Color, Element, Length, Subscription, Task, padding, theme,
+};
+use iced_layershell::build_pattern::application;
+use iced_layershell::reexport::Anchor;
+use iced_layershell::settings::LayerShellSettings;
+use iced_layershell::to_layer_message;
+use std::process::Stdio;
+use std::time::Instant;
+use tokio::process::Command;
+
+#[allow(unused)]
+#[to_layer_message]
+#[derive(Debug, Clone)]
+enum Message {
+    Tick(Instant),
+    SetWorkspace(u32),
+    OpenOverview,
+    OpenPavucontrol,
+    SetDisplay(Display),
+    NoOp,
+}
+
+struct State {
+    wifi_handle: svg::Handle,
+    audio_handle: svg::Handle,
+    power_handle: svg::Handle,
+    display: Display,
+}
+
+#[derive(Debug, Clone)]
+struct Display {
+    wifi_str: String,
+    audio_str: String,
+    power_str: String,
+    current_workspace: u32,
+    date_time: DateTime<Local>,
+}
+
+const SVG_SIZE: u32 = 16;
+
+fn bg_color() -> Background {
+    Background::Color(Color::from_rgb8(100, 100, 50))
+}
+
+fn main() -> iced_layershell::Result {
+    application(init, namespace, update, view)
+        .layer_settings(LayerShellSettings {
+            size: Some((1920, 32)),
+            anchor: Anchor::Bottom | Anchor::Left | Anchor::Right,
+            margin: (0, 10, 0, 10),
+            ..Default::default()
+        })
+        .subscription(subscriptions)
+        .style(app_style)
+        .run()
+}
+
+fn subscriptions(_state: &State) -> Subscription<Message> {
+    iced::time::every(milliseconds(64)).map(Message::Tick)
+}
+
+fn namespace() -> String {
+    "Hail bar".to_string()
+}
+
+fn init() -> (State, Task<Message>) {
+    let wifi_handle =
+        svg::Handle::from_path(format!("{}/resources/wifi.svg", env!("CARGO_MANIFEST_DIR")));
+
+    let audio_handle = svg::Handle::from_path(format!(
+        "{}/resources/speaker.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+
+    let power_handle = svg::Handle::from_path(format!(
+        "{}/resources/power.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+
+    let display = Display {
+        wifi_str: "-".into(),
+        audio_str: "-".into(),
+        power_str: "-".into(),
+        current_workspace: 1,
+        date_time: Local::now(),
+    };
+
+    let state = State {
+        wifi_handle,
+        audio_handle,
+        power_handle,
+        display,
+    };
+
+    (state, Task::none())
+}
+
+fn app_style(_state: &State, _theme: &iced::Theme) -> theme::Style {
+    theme::Style {
+        background_color: Color::TRANSPARENT,
+        text_color: Color::WHITE,
+    }
+}
+
+fn update(state: &mut State, message: Message) -> Task<Message> {
+    match message {
+        Message::Tick(_) => Task::future(async {
+            let display = Display {
+                date_time: Local::now(),
+                wifi_str: get_wifi_str().await.unwrap_or("? WiFi".into()),
+                audio_str: get_audio_str().await.unwrap_or("? audio".into()),
+                power_str: get_power_str().await.unwrap_or("? battery".into()),
+                current_workspace: get_current_workspace().await.unwrap_or(1),
+            };
+
+            Message::SetDisplay(display)
+        }),
+        Message::SetWorkspace(index) => Task::future(async move {
+            let _ = set_workspace(index).await;
+            Message::NoOp
+        }),
+        Message::OpenOverview => Task::future(async {
+            let _ = open_overview().await;
+            Message::NoOp
+        }),
+        Message::OpenPavucontrol => Task::future(async {
+            let _ = open_pavucontrol().await;
+            Message::NoOp
+        }),
+        Message::SetDisplay(display) => {
+            state.display = display;
+
+            Task::none()
+        }
+        Message::NoOp => Task::none(),
+        _ => panic!("Not implemented"),
+    }
+}
+
+async fn open_pavucontrol() -> tokio::io::Result<()> {
+    Command::new("pavucontrol")
+        .stdout(Stdio::inherit())
+        .output()
+        .await?;
+
+    Ok(())
+}
+
+async fn open_overview() -> tokio::io::Result<()> {
+    Command::new("niri")
+        .args(["msg", "action", "toggle-overview"])
+        .stdout(Stdio::piped())
+        .output()
+        .await?;
+
+    Ok(())
+}
+
+async fn set_workspace(index: u32) -> tokio::io::Result<()> {
+    Command::new("niri")
+        .args(["msg", "action", "focus-workspace", &index.to_string()])
+        .stdout(Stdio::piped())
+        .output()
+        .await?;
+
+    Ok(())
+}
+
+async fn get_current_workspace() -> tokio::io::Result<u32> {
+    let mut niri = Command::new("niri")
+        .args(["msg", "workspaces"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let niri_out: Stdio = niri
+        .stdout
+        .take()
+        .unwrap()
+        .try_into()
+        .expect("failed to convert to Stdio");
+
+    let rg = Command::new("rg")
+        .args(["\\*", "-r", "$1"])
+        .stdin(niri_out) // Pipe ls_output to grep's stdin
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let output = rg.wait_with_output().await?;
+
+    let err = io::Error::new(io::ErrorKind::InvalidData, "Expected an integer");
+    String::from_utf8_lossy(&output.stdout)
+        .trim()
+        .parse()
+        .map_err(|_| err)
+}
+
+async fn get_wifi_str() -> io::Result<String> {
+    let mut wifi = Command::new("iwctl")
+        .args(["station", "wlan0", "show"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let wifi_out: Stdio = wifi
+        .stdout
+        .take()
+        .unwrap()
+        .try_into()
+        .expect("failed to convert to Stdio");
+
+    let rg = Command::new("rg")
+        .args(["Connected network", "-r", "$1"])
+        .stdin(wifi_out) // Pipe ls_output to grep's stdin
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let output = rg.wait_with_output().await?;
+
+    Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
+}
+
+async fn get_audio_str() -> io::Result<String> {
+    let pamixer = Command::new("pamixer")
+        .arg("--get-volume")
+        .stdout(Stdio::piped())
+        .output()
+        .await?;
+
+    Ok(String::from_utf8_lossy(&pamixer.stdout).trim().to_string())
+}
+
+async fn get_power_str() -> io::Result<String> {
+    let battery = Command::new("cat")
+        .arg("/sys/class/power_supply/BAT1/capacity")
+        .stdout(Stdio::piped())
+        .output()
+        .await?;
+
+    Ok(String::from_utf8_lossy(&battery.stdout).trim().to_string())
+}
+
+fn view(state: &State) -> Element<'_, Message> {
+    container(
+        row![
+            outer(workspaces(state)).align_x(Alignment::Start),
+            outer(time_date(state)).align_x(Alignment::Center),
+            outer(info(state)).align_x(Alignment::End)
+        ]
+        .width(Length::Fill)
+        .spacing(10),
+    )
+    .center_y(32)
+    .padding(padding::left(5).right(5))
+    .style(style_container)
+    .into()
+}
+
+fn outer(elem: Element<'_, Message>) -> Container<'_, Message> {
+    container(elem).width(Length::Fill)
+}
+
+fn workspaces(state: &State) -> Element<'_, Message> {
+    let display = &state.display;
+    let binoculars_handle = svg::Handle::from_path(format!(
+        "{}/resources/binoculars.svg",
+        env!("CARGO_MANIFEST_DIR")
+    ));
+
+    let button_style = |btn_ws_index: u32| -> button::Style {
+        if display.current_workspace == btn_ws_index {
+            button::Style {
+                background: Some(Color::from_rgb8(0, 167, 119).into()),
+                border: Border {
+                    color: Color::BLACK,
+                    width: 2.,
+                    radius: Radius::new(2.),
+                },
+                ..Default::default()
+            }
+        } else {
+            button::Style {
+                text_color: Color::from_rgb8(75, 75, 75),
+                background: Some(Color::from_rgb8(11, 11, 11).into()),
+                border: Border {
+                    color: Color::BLACK,
+                    width: 2.,
+                    radius: Radius::new(5),
+                },
+                ..Default::default()
+            }
+        }
+    };
+
+    container(
+        row![
+            button(svg(binoculars_handle).width(24).height(32))
+                .style(|_, _| button::Style {
+                    background: Some(Color::from_rgb8(0, 167, 119).into()),
+                    border: Border {
+                        color: Color::BLACK,
+                        width: 2.,
+                        radius: Radius::new(5),
+                    },
+                    ..Default::default()
+                })
+                .on_press(Message::OpenOverview),
+            button("1")
+                .on_press(Message::SetWorkspace(1))
+                .style(move |_theme, _status| button_style(1))
+                .width(Length::Shrink),
+            button("2")
+                .on_press(Message::SetWorkspace(2))
+                .style(move |_theme, _status| button_style(2))
+                .width(Length::Shrink),
+            button("3")
+                .on_press(Message::SetWorkspace(3))
+                .style(move |_theme, _status| button_style(3))
+                .width(Length::Shrink),
+        ]
+        .align_y(Alignment::Center)
+        .height(32)
+        .spacing(5),
+    )
+    .align_x(Alignment::Start)
+    .width(Length::Fill)
+    .into()
+}
+
+fn time_date(state: &State) -> Element<'_, Message> {
+    let formatted = format!("{}", state.display.date_time.format("%d/%m/%Y %H:%M"));
+
+    container(row![
+        text(formatted)
+            .align_y(Alignment::Center)
+            .height(32)
+            .align_x(Alignment::Center)
+    ])
+    .align_x(Alignment::Center)
+    .width(Length::Shrink)
+    .padding(padding::right(10).left(10))
+    .style(module_style)
+    .into()
+}
+
+fn info(state: &State) -> Element<'_, Message> {
+    let display = &state.display;
+
+    container(
+        row![
+            module(row![
+                svg(state.wifi_handle.clone())
+                    .width(SVG_SIZE)
+                    .height(SVG_SIZE),
+                text(&display.wifi_str)
+            ]),
+            module_button(row![
+                svg(state.audio_handle.clone())
+                    .width(SVG_SIZE)
+                    .height(SVG_SIZE),
+                text(&display.audio_str)
+            ])
+            .on_press(Message::OpenPavucontrol),
+            module(row![
+                svg(state.power_handle.clone())
+                    .width(SVG_SIZE)
+                    .height(SVG_SIZE),
+                text(&display.power_str)
+            ]),
+        ]
+        .spacing(5),
+    )
+    .align_x(Alignment::End)
+    .width(Length::Shrink)
+    .padding(padding::right(10).left(10))
+    .into()
+}
+
+fn module_button(row: iced::widget::Row<'_, Message>) -> Button<'_, Message> {
+    button(
+        row.spacing(10)
+            .width(Length::Shrink)
+            .align_y(Alignment::Center)
+            .height(32),
+    )
+    .padding(padding::right(10).left(10))
+    .style(button_module_style)
+}
+fn module(row: iced::widget::Row<'_, Message>) -> Container<'_, Message> {
+    container(
+        row.spacing(10)
+            .width(Length::Shrink)
+            .align_y(Alignment::Center)
+            .height(32),
+    )
+    .align_x(Alignment::Center)
+    .padding(padding::right(10).left(10))
+    .style(module_style)
+}
+
+fn button_module_style(_: &iced::Theme, _: button::Status) -> button::Style {
+    button::Style {
+        text_color: Color::WHITE,
+        background: Some(bg_color()),
+        border: Border {
+            color: Color::BLACK,
+            width: 2.,
+            radius: Radius::new(5.),
+        },
+        ..Default::default()
+    }
+}
+
+fn module_style(_: &iced::Theme) -> container::Style {
+    container::Style {
+        background: Some(bg_color()),
+        border: Border {
+            color: Color::BLACK,
+            width: 2.,
+            radius: Radius::new(5.),
+        },
+        ..Default::default()
+    }
+}
+
+fn style_container(_theme: &iced::Theme) -> container::Style {
+    container::Style {
+        background: Some(Background::Color(Color::TRANSPARENT)),
+        border: Border::default().width(1),
+        ..Default::default()
+    }
+}
